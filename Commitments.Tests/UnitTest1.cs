60f5f1f610ee98979c6f46c5fc039a68123b2f88
jurzon <jurzon@users.noreply.github.com>
2025-08-27 13:28:38 +0200

    Tests: rename placeholder test method to avoid CA1707

--- Commitments.Tests/UnitTest1.cs
+++ Commitments.Tests/UnitTest1.cs
@@ -1,10 +1,12 @@
+using FluentAssertions;
+
 namespace Commitments.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void PlaceholderWorks()
     {
-
+        true.Should().BeTrue();
     }
 }
